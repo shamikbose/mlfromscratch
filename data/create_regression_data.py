f9c18028fe4931eccacc441593a81e79226ea6a3
shamikbose <shamik.bose89@gmail.com>
2022-03-06 13:21:48 -0500

    Linear Regression complete

--- data/create_regression_data.py
+++ data/create_regression_data.py
@@ -1,24 +1,24 @@
-import random
+
 import numpy as np
-def create_data_points(n: int, x_limit=1000, y_limit=100, seed = None, verbose=False) -> np.array:
-    '''
-    Create a 2-D numpy array with n elements. If x_limit and y_limit are specified, the (x,y) tuples are in those limits
-    Provide seed to be reproducible
-    args: 
-    n: number of points to return
-    x_limit: the range in which to produce the x-values. The lower range is negative of this value (optional)
-    y_limit: the range in which to produce the y-values. The lower range is negative of this value (optional)
-    verbose: if True, the data points generated will be shown to the user
-    return:
-    2-D numpy array
-    '''
-    if n<=0:
-        raise ValueError("n has to be a positive number")
-    if x_limit<=0 or y_limit<=0:
-        raise ValueError("x_limit and y_limit can take positive values only")
-    x_vals = np.random.uniform(-x_limit, x_limit, n)
-    y_vals = np.random.uniform(-y_limit, y_limit, n)
-    data_points = np.dstack((x_vals, y_vals))
-    if verbose:
-        print(data_points)
-    return data_points
+class LinearRegressionData:
+    def create_data_points(n: int, seed = None, verbose=False) -> np.array:
+        '''
+        Create a 2-D numpy array with n elements. If x_limit and y_limit are specified, the (x,y) tuples are in those limits
+        Provide seed to be reproducible
+        args: 
+        n: number of points to return
+        verbose: if True, the data points generated will be shown to the user
+        return:
+        2-D numpy array
+        '''
+        if n<=0:
+            raise ValueError("n has to be a positive number")
+        x_vals = np.random.uniform(0, 100, n)
+        x_coeff = np.random.uniform(0, 10)
+        constant = np.random.uniform(0, 10)
+        y_function = lambda x: x_coeff*x + constant
+        y_vals = np.array([y_function(x) + np.random.uniform(-100,100) for x in x_vals])
+        if verbose:
+            print("Slope: {}\nConstant: {}".format(x_coeff, constant))
+            print(x_vals, y_vals)
+        return x_vals, y_vals
